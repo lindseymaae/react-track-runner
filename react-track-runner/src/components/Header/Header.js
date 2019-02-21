c5817e1e72dbf1e6329ba943ac6dc06eeb2851bf
Lindsey Sandberg <sandbergy@yahoo.com>
2019-02-20 21:49:29 -0600

    added base files

---
+++ react-track-runner/src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+            <header>React Track Runner</header>
+        )}
+}
+
+export default Header;
